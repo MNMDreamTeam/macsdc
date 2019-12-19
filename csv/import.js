4a811752a19a17c4196b377c388c23c4917ab528
mcolligan <mcolligan1@gmail.com>
2019-12-18 22:03:46 -0700

    insert working on importer

--- csv/import.js
+++ csv/import.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const fastcsv = require('fast-csv');
 
-let questions = fs.createReadStream('csv/questions_test.csv');
+let questions = fs.createReadStream('/Users/mcolligan/FEC/projectGreenfieldII/csvData/testFiles/answers_test.csv');
 let parseData = [];
 
 let csvStream = fastcsv.parse()
@@ -9,7 +9,37 @@ let csvStream = fastcsv.parse()
   parseData.push(data);
 })
 .on('end', () => {
+  const Pool = require('pg').Pool;
+
   parseData.shift();
+
+  const pool = new Pool ({
+    host: 'localhost',
+    user: 'mcolligan',
+    database: 'testapi',
+    port: 5432
+  });
+
+  const questionInsert = 'INSERT INTO questions (id, product_id, body, date_written, asker_name, asker_email, reported, helpful) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)';
+
+  const answersInsert = 'INSERT INTO answers (id, question_id, body, date_written, answerer_name, answerer_email, reported, helpful) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)';
+
+  pool.connect((err, client, done) => {
+    if (err) throw err;
+    try {
+      parseData.forEach(el => {
+        client.query(answersInsert, el, (err, res) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(`inserted ${res} @ ${el}`);
+          }
+        })
+      })
+    } finally {
+      done();
+    }
+  })
 });
 
 questions.pipe(csvStream);
