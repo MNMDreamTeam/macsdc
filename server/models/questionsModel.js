abb4456248552beae86b2f59bede76ad765a4c6e
mcolligan <mcolligan1@gmail.com>
2019-12-31 16:10:16 -0700

    get route for Q/A/P

--- server/models/questionsModel.js
+++ server/models/questionsModel.js
@@ -2,22 +2,22 @@ const qdb = require('../db/index.js');
 const { qt, at, apt } = require('../../config.js');
 
 const getAnswers = (qId) => {
-  return new Promise( (resolve, reject) => {
+  return new Promise((resolve, reject) => {
     let q = `SELECT * FROM ${at} WHERE question_id = $1`;
     let p = [qId];
 
-    qdb.query(q, p).then( (res) => {
+    qdb.query(q, p).then((res) => {
       resolve(res);
     })
   })
 }
 
 const getPhotos = (aId) => {
-  return new Promise( (resolve, reject) => {
+  return new Promise((resolve, reject) => {
     let q = `SELECT * FROM ${apt} WHERE answer_id = $1`;
     let p = [aId];
 
-    qdb.query(q, p).then( (res) => {
+    qdb.query(q, p).then((res) => {
       resolve(res);
     })
   })
@@ -28,32 +28,40 @@ const queryQuestions = (product_id, count) => {
     product_id: product_id,
   }
 
-  return new Promise( (resolve, reject) => {
+
+  return new Promise((resolve, reject) => {
     let q = `SELECT * FROM ${qt} WHERE product_id = $1 AND reported = $2 ORDER BY question_id LIMIT $3`;
     let p = [product_id, 0, count];
 
-    qdb.query(q, p).then( (questions) => {
+    qdb.query(q, p).then((questions) => {
       let answersArr = [];
       output.results = questions;
-      output.results.forEach( (el, i) => {
+      output.results.forEach((el, i) => {
         output.results[i].answers = {};
         answersArr.push(getAnswers(el.question_id));
       });
-      Promise.all(answersArr).then( (answers) => {
+      Promise.all(answersArr).then((answers) => {
         let photos = [];
-        answers.forEach( (ans, j) => { // fix this nested loop, no need for it
-          ans.forEach( (obj) => {
+        answers.forEach((ans, j) => { // fix this nested loop, no need for it
+          ans.forEach((obj) => {
             output.results[j].answers[obj.id] = obj;
             output.results[j].answers[obj.id].photos = [];
             photos.push(getPhotos(obj.id));
           })
         });
-        Promise.all(photos).then( (ansPhotos) => {
-          ansPhotos.forEach( (photo, h) => {
-            if (photo.length > 0) {
-              console.log(output.results.answers[photo.answer_id]);
-            }
-          })
+        Promise.all(photos).then((ansPhotos) => {
+            ansPhotos.forEach((photo, i) => {
+              if (photo.length > 0) {
+                // I'm very sorry for this.....but here we go
+                photo.forEach((el, j) => {
+                  output.results.forEach((q, k) => {
+                    if (q.answers.hasOwnProperty(el.answer_id)) {
+                      q.answers[el.answer_id].photos.push(el.url);
+                    }
+                  })
+                })
+              }
+            })
           resolve(output);
         })
       });
