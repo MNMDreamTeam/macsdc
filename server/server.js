59b6e6493cb067f72d22b1ba2b9da3c3515b30ec
mcolligan <mcolligan1@gmail.com>
2020-01-03 15:57:22 -0700

    metrics and load testing

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 
 const questionsRoutes = require('./routes/questionsRoutes.js');
 const answerRoutes = require('./routes/answerRoutes.js');
+// require('newrelic'); - for metrics
 
 const app = express();
 
