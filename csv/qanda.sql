9f619b887f326f541e005a485f140c6fdf5b1cbd
mcolligan <mcolligan1@gmail.com>
2020-01-06 10:20:16 -0700

    update to sql file

--- csv/qanda.sql
+++ csv/qanda.sql
@@ -31,6 +31,13 @@ id serial PRIMARY KEY,
 answer_id int REFERENCES answers(id),
 url VARCHAR(400));
 
+CREATE INDEX q_id ON qa.questions (question_id);
+
+CREATE INDEX a_id ON qa.answers (answer_id);
+CREATE INDEX aq_id ON qa.answers (question_id);
+
+CREATE INDEX ap_id ON qa.answers_photos (id);
+CREATE INDEX apa_id ON qa.answers_photos (answer_id);
 
 COPY qa.questions(question_id, product_id, question_body, question_date, asker_name, asker_email,reported, question_helpfulness)
 FROM '/Users/mcolligan/FEC/projectGreenfieldII/csvData/questions.csv' DELIMITER ',' CSV HEADER;
@@ -40,3 +47,11 @@ FROM '/Users/mcolligan/FEC/projectGreenfieldII/csvData/answers.csv' DELIMITER ',
 
 COPY qa.answers_photos(id, answer_id, url)
 FROM '/Users/mcolligan/FEC/projectGreenfieldII/csvData/answers_photos.csv' DELIMITER ',' CSV HEADER;
+
+SELECT setval('questions_id_seq', (SELECT MAX(question_id) FROM qa.questions)+1);
+
+SELECT setval('answers_id_seq', (SELECT MAX(answer_id) FROM qa.answers)+1);
+
+SELECT setval('answers_photos_id_seq', (SELECT MAX(id) FROM qa.answers_photos)+1);
+
+
